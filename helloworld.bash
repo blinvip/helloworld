fd8c19f36fe26758271a2a7af9625782e3cd735a
Bogdan Blinov @BlinGuyq <blinvip@mail.ru>
2021-08-16 08:59:07 -0300

    First commit

---
+++ helloworld.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: Bogdan
+# Date Created: 30/04/2021
+# Description: This script will output "Hello World" on the screen
+# Date Modified: 30/04/2021
+
+echo
+echo Hello World
+echo
